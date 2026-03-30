04efa8924f07a6bedc9c2fe790fbf9cf0b53c3df
Abdo Hanoun <abdulelah.hanoun@chasacademy.se>
2026-03-30 02:36:53 +0200

    fix: lowercase resource names for GCP compliance

--- variables.tf
+++ variables.tf
@@ -6,7 +6,7 @@ variable "project_id" {
 variable "region" {
   description = "GCP Region"
   type        = string
-  default = "europe-west1"
+  default = "us-central1"
 }
 
 variable "student_id" {
